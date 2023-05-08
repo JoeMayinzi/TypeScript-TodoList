b42d9f9d7d0d489a15be95c73ed44be2ff07afa9
JoeMayinzi <joemayinzi@gmail.com>
2023-05-08 23:18:59 +0200

    Feature: display todoList

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,8 @@
 "use strict";
 const input = document.getElementById("form2");
 const addButton = document.getElementById("addButton");
+const todoContainer = (document.getElementById("todoContainer"));
+const noTodoText = document.getElementById("noTodo");
 let value = "";
 const todos = [];
 // add todo on the todo button click
@@ -9,7 +11,26 @@ const addTodo = (event) => {
     value = input.value.toString();
     todos.push(value);
     input.value = "";
-    console.log(todos);
+    displayTodo(todos);
 };
 // Display todo on the web page
+const displayTodo = (todos) => {
+    todos.map((todo) => {
+        if (todos.length) {
+            todoContainer.removeChild(noTodoText);
+            todoContainer.insertAdjacentHTML("beforebegin", `<li
+           class="list-group-item d-flex align-items-center border-0 mb-2 rounded"
+            style="background-color: #f4f6f7"
+        >
+            <input
+              class="form-check-input me-2"
+              type="checkbox"
+              value=""
+              aria-label="..."
+              />
+            <span>${todo}</span>
+      </li>`);
+        }
+    });
+};
 addButton.addEventListener("click", addTodo);
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,12 @@ type todoList = string[];
 
 const input = <HTMLInputElement>document.getElementById("form2");
 const addButton = <HTMLButtonElement>document.getElementById("addButton");
+const todoContainer = <HTMLUListElement>(
+  document.getElementById("todoContainer")
+);
+
+const noTodoText = document.getElementById("noTodo") as HTMLParagraphElement;
+
 let value: InputValue = "";
 const todos: todoList = [];
 
@@ -13,9 +19,33 @@ const addTodo = (event: Event): void => {
   value = input.value.toString();
   todos.push(value);
   input.value = "";
-  console.log(todos);
+
+  displayTodo(todos);
 };
 
 // Display todo on the web page
+
+const displayTodo = (todos: string[]) => {
+  todos.map((todo) => {
+    if (todos.length) {
+      todoContainer.removeChild(noTodoText);
+      todoContainer.insertAdjacentHTML(
+        "beforebegin",
+        `<li
+           class="list-group-item d-flex align-items-center border-0 mb-2 rounded"
+            style="background-color: #f4f6f7"
+        >
+            <input
+              class="form-check-input me-2"
+              type="checkbox"
+              value=""
+              aria-label="..."
+              />
+            <span>${todo}</span>
+      </li>`
+      );
+    }
+  });
+};
 
 addButton.addEventListener("click", addTodo);
