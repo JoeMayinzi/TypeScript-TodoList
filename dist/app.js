5507633b9e95a55ab5fa14a544cf1bebba2eb146
JoeMayinzi <joemayinzi@gmail.com>
2023-05-08 17:32:11 +0200

    get todo list

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("Hello TypeScript first project");
+const input = document.getElementById("form2");
+const addButton = document.getElementById("addButton");
+let value = "";
+const todos = [];
+// add todo on the todo button click
+const addTodo = (event) => {
+    event.preventDefault();
+    value = input.value.toString();
+    todos.push(value);
+    input.value = "";
+    console.log(todos);
+};
+// Display todo on the web page
+addButton.addEventListener("click", addTodo);
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log("Hello TypeScript first project");
+type InputValue = string;
+type todoList = string[];
+
+const input = <HTMLInputElement>document.getElementById("form2");
+const addButton = <HTMLButtonElement>document.getElementById("addButton");
+let value: InputValue = "";
+const todos: todoList = [];
+
+// add todo on the todo button click
+
+const addTodo = (event: Event): void => {
+  event.preventDefault();
+  value = input.value.toString();
+  todos.push(value);
+  input.value = "";
+  console.log(todos);
+};
+
+// Display todo on the web page
+
+addButton.addEventListener("click", addTodo);
